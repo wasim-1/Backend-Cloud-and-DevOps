d45731a1f20b95f89b6742cf7850aa2ae8a58358
Wasim Akram <128415984+wasim-1@users.noreply.github.com>
2025-02-07 13:55:38 +0000

    Add equalityAndTypes

---
+++ equalityAndTypes.js
@@ -0,0 +1,12 @@
+// Equality Comparisons
+console.log(1 == "Wasim"); // 1 == NaN
+console.log(NaN === NaN);
+
+// Typeof Operator
+console.log(typeof "1");
+console.log(typeof 1);
+console.log(typeof false);
+console.log(typeof undefined);
+
+// Null Type
+console.log(typeof null)
